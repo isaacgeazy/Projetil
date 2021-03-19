092c39e411b4708664fac681fa11e6d79e00de24
Isaac <isaacgeazy@gmail.com>
2021-03-19 20:46:43 -0300

    Projétil

--- Program.cs
+++ Program.cs
@@ -7,29 +7,22 @@ namespace Projetil
         static void Main(string[] args)
         {
            const double g = 9.80665;
-           double ang;
-           double vel;
-           double rad;
-           double h;
-           double x;
+           double ang, rad, vel, h, x;
 
-           Console.WriteLine("---Projétil---");
-
-           Console.Write("Entre com a velocidade, em m/s ..:");
+           Console.Write(" VELOCIDADE ..:");
            vel = Convert.ToDouble(Console.ReadLine());
 
-           Console.Write("Entre com ÂNGULO..: ");
+           Console.Write(" ÂNGULO..: ");
            ang = Convert.ToDouble(Console.ReadLine());
 
            rad = ang * (Math.PI / 180);
-
            x = Math.Pow(vel, 2) * Math.Sin(2 * rad) / g;
-           Console.WriteLine($"Alcance maximo ..: {x:N2} m/s");
-
            h = Math.Pow((vel * Math.Sin(rad)), 2) / (2 * g);
-           Console.WriteLine($"Altura maxima ..: {h:N2} m/s");
+           
+           Console.WriteLine();
 
-
+           Console.WriteLine($"Alcance máximo: {x:N2} m/s");
+           Console.WriteLine($"Altura máxima: {h:N2} m/s");
         }
     }
 }
