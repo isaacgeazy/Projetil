aa20a7097dd741d7d4369f7c66db2f2db98f2ae9
Isaac <isaacgeazy@gmail.com>
2021-03-19 20:38:07 -0300

    Atividade Projetil

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Projetil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const double g = 9.80665;
+           double ang;
+           double vel;
+           double rad;
+           double h;
+           double x;
+
+           Console.WriteLine("---Projétil---");
+
+           Console.Write("Entre com a velocidade, em m/s ..:");
+           vel = Convert.ToDouble(Console.ReadLine());
+
+           Console.Write("Entre com ÂNGULO..: ");
+           ang = Convert.ToDouble(Console.ReadLine());
+
+           rad = ang * (Math.PI / 180);
+
+           x = Math.Pow(vel, 2) * Math.Sin(2 * rad) / g;
+           Console.WriteLine($"Alcance maximo ..: {x:N2} m/s");
+
+           h = Math.Pow((vel * Math.Sin(rad)), 2) / (2 * g);
+           Console.WriteLine($"Altura maxima ..: {h:N2} m/s");
+
+
+        }
+    }
+}
